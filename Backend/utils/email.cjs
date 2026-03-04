4d56e9a9a721f6e279fb563d5eeaddb9ecee0e1f
rathorePriyanshu <rathorepriyansh2003@gmail.com>
2026-03-05 00:02:27 +0530

    fix email

--- Backend/utils/email.cjs
+++ Backend/utils/email.cjs
@@ -2,14 +2,12 @@ const nodemailer = require("nodemailer");
 
 
 const transport = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
-    }
+    },
+    family: 4
 });
 
 module.exports.transport = transport;
